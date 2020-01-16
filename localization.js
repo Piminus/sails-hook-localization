05d1b9073082d0dc3f9b22496b8344afa12812f2
Piminus <raidervolt@gmail.com>
2020-01-17 00:18:38 +0200

    Config file

---
+++ localization.js
@@ -0,0 +1,27 @@
+module.exports.localization = {
+  models: ['blabla', 'testmodel'],
+  default_locale: function (model, cb) {
+    switch (model) {
+      case 'blabla':
+        cb('en');
+        break;
+      case 'testmodel':
+        cb('ru');
+        break;
+      default:
+        break;
+    }
+  },
+  locales: function (model, cb) {
+    switch (model) {
+      case 'blabla':
+        cb(['en', 'uk']);
+        break;
+      case 'testmodel':
+        cb(['ru', 'ua']);
+        break;
+      default:
+        break;
+    }
+  },
+};
